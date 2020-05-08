3f632211cd6982da2cdaad9671b8c4a91833bc1c
sean <1403273045@qq.com>
2020-05-08 16:54:48 +0800

    新增快速排序

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Future;
+
 public class Main {
 
     public static void main(String[] args) {
+
         System.out.println("Hello World!");
     }
 }
