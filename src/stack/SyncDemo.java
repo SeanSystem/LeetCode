34d6b2ab6e0adf06c5e35f7edb2fb1a92d2eb99e
sean <1403273045@qq.com>
2020-05-18 21:35:29 +0800

    新增N皇后问题

---
+++ src/stack/SyncDemo.java
@@ -0,0 +1,22 @@
+package stack;
+
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * 测试Synchronized
+ *
+ * @author by Sean
+ * @date 2020/5/13 23:15
+ */
+public class SyncDemo {
+
+    public synchronized void test(){
+        System.out.println("123");
+    }
+
+    public void test2(){
+        synchronized (SyncDemo.class){
+            System.out.println("123");
+        }
+    }
+}
