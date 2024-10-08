02382f075945146f90ed003a28b8872bf8c5a528
sean <1403273045@qq.com>
2024-10-08 23:36:42 +0800

    hot100刷题

---
+++ src/hot100/MajorityElement.java
@@ -0,0 +1,32 @@
+package hot100;
+
+/**
+ * 给定一个大小为 n 的数组 nums ，返回其中的多数元素。多数元素是指在数组中出现次数 大于 ⌊ n/2 ⌋ 的元素。
+ * <p>
+ * 你可以假设数组是非空的，并且给定的数组总是存在多数元素。
+ * <p>
+ * <p>
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：nums = [3,2,3]
+ * 输出：3
+ * 示例 2：
+ * <p>
+ * 输入：nums = [2,2,1,1,1,2,2]
+ * 输出：2
+ */
+public class MajorityElement {
+
+    public int majorityElement(int[] nums) {
+        int count = 0;
+        Integer candidate = null;
+        for (int num : nums) {
+            if (count == 0) {
+                candidate = num;
+            }
+            count += (num == candidate) ? 1 : -1;
+        }
+        return candidate;
+    }
+}
